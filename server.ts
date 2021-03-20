35a65b21e0704510b9af21b1f28e7bef99c381cb
Gavin <gavinbrrtt@gmail.com>
2021-03-19 21:28:35 -0700

    Updated port to 8000.

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = 8000;
 const { readPostFromDisk } = require("./database/diskUtilities.ts");
 const database = require("./database/databaseUtilities.ts");
 
